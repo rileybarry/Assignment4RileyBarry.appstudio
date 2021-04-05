9cd2df356735e1aeb4e25f6f694cac99df61c844
rileybarry <60715866+rileybarry@users.noreply.github.com>
2021-04-05 14:35:47 -0500

    working dropdown

--- forms/customerInfo/customerInfo.js
+++ forms/customerInfo/customerInfo.js
@@ -6,9 +6,10 @@ let results = []
 let stateArr = []
 
 customerInfo.onshow = function() {
-  query = "SELECT state FROM customer"
+  drpStates.clear()
+  query = "SELECT DISTINCT state FROM customer ORDER BY state ASC;"
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
-console.log(req)
+  console.log(req)
 
   if (req.status == 200) { //transit trip worked. 
     results = JSON.parse(req.responseText)
@@ -16,17 +17,20 @@ console.log(req)
       console.log('There are no states in database')
     else {
       // store results values into new array as string
-      for (i=0;i<results.length;i++)
+      for (i = 0; i < results.length; i++) {
         stateArr.push(results[i].toString())
-      
-      var state = ''
-      for (i = 0; i < stateArr.length; i++) {
-        state = stateArr.includes(stateArr[i], i + 1)
-        console.log(state)
-        if (state == false)
-          drpStates.addItem(stateArr[i])
-        }
+        drpStates.addItem(stateArr[i])
+      }
     } // end if else
   } else // transit didn't work
     console.log(`Error code: ${req.status}`)
+}
+
+
+drpStates.onclick=function(s){
+  if(typeof(s) == 'object')
+    return
+  else {
+    drpStates.value = s
+  }
 }
