1e205a42b3c0f632a3e0e185d7f0e3772ce039d2
rileybarry <60715866+rileybarry@users.noreply.github.com>
2021-04-05 15:05:20 -0500

    completed first form

--- forms/customerInfo/customerInfo.js
+++ forms/customerInfo/customerInfo.js
@@ -4,12 +4,12 @@ let netID = 'rwb12128'
 let pw = 'Speedyrwb645'
 let results = []
 let stateArr = []
+let stateLetters = []
 
 customerInfo.onshow = function() {
   drpStates.clear()
   query = "SELECT DISTINCT state FROM customer ORDER BY state ASC;"
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
-  console.log(req)
 
   if (req.status == 200) { //transit trip worked. 
     results = JSON.parse(req.responseText)
@@ -27,10 +27,35 @@ customerInfo.onshow = function() {
 }
 
 
-drpStates.onclick=function(s){
-  if(typeof(s) == 'object')
+drpStates.onclick = function(s) {
+  if (typeof(s) == 'object')
     return
   else {
+    lstEmployees.clear()
     drpStates.value = s
+
+    //new query
+    query = "SELECT name, state FROM customer GROUP BY state ASC;"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+
+    if (req.status == 200) { //transit trip worked. 
+      results = JSON.parse(req.responseText)
+      if (results.length == 0)
+        console.log('There are no names or states in the database')
+        
+      else {
+
+        for (i = 0; i < results.length; i++) 
+          stateLetters.push(results[i].toString())
+
+        for (i = 0; i < results.length; i++) {
+          let letters = stateLetters[i].slice(-2)
+          
+          if (letters == s)
+            lstEmployees.addItem(stateLetters[i])
+        }
+      }
+    } else // transit didn't work
+      console.log(`Error code: ${req.status}`)
   }
 }
