0d2127f2ab2f75c5284155ba47d6260e1152dcd4
rileybarry <60715866+rileybarry@users.noreply.github.com>
2021-04-05 15:32:26 -0500

    update form 2

--- forms/Form1/Form1.js
+++
@@ -1,9 +0,0 @@
-/*
-var arr = ['one','two','three','one','one','two','four']
-
-for(i=0;i<arr.length;i++)
-  if (arr.includes(arr[i], i + 1))
-    console.log('contains variable')
-  else
-    console.log('does not contain variable')
-*/
---
+++ forms/employeeNames/employeeNames.js
@@ -0,0 +1,36 @@
+var namesArr = []
+
+btnDisplay.onclick=function(){
+  lstDisplay.clear()
+  query = "SELECT name FROM customer ORDER BY name ASC;"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+  
+  if (req.status == 200) { //transit trip worked. 
+    results = JSON.parse(req.responseText)
+    if (results.length == 0)
+      console.log('There are no names in database')
+    else {
+      // store results values into new array as string
+      for (i = 0; i < results.length; i++) {
+        namesArr.push(results[i].toString())
+        lstDisplay.addItem(namesArr[i])
+      }
+    } // end if else
+  } else // transit didn't work
+    console.log(`Error code: ${req.status}`)
+}
+
+
+lstDisplay.onclick=function(){
+  query = `DELETE FROM customer WHERE name=${lstDisplay.value};`
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+  
+  if (req.status == 200) { //transit trip worked. 
+    results = JSON.parse(req.responseText)
+    if (results.length == 0) {
+      console.log('That name is not in the database')
+    }
+  } else {
+    console.log(`Error code: ${req.status}`)
+  }
+}
